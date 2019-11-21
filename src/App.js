a034eec00a73063c0b74bdd3b3d364b87ab1bf0b
William Wong <williamwdev@gmail.com>
2019-11-21 15:05:06 -0800

    initial commit, starting point

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="App">
+      <body>
+        <h4>Welcome!</h4>
+        If you are here, then you are ready to start coding the example!
+      </body>
+    </main>
+  );
+}
+
+export default App;
