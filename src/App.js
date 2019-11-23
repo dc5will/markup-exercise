fa2f35e413524b158a932733701c708824248ac5
William Y Wong <wwong1987@gmail.com>
2019-11-22 23:41:27 -0800

    working on fetching venetian location name and venetian img

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Hotels from './components/Hotels/Hotels';
 import Description from "./components/Description/Description";
+import HotelInfo from './components/HotelInfo/HotelInfo';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <Hotels/>
         <Description/>
+        <HotelInfo/>
     </main>
   );
 }
