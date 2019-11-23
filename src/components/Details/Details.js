7b647e592f7d7d2a59dc2d9f23acc8ecf572405a
William Y Wong <wwong1987@gmail.com>
2019-11-23 14:52:20 -0800

    details container basic set up, need to figure out how to extract label and values

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import HotelApiService from "../../services/hotel-api-service";
 import './Details.css'
 
 export default function Details() {
+  const [venetianDetails, setVenetianDetails] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    HotelApiService.getVenetianInfo().then(data => {
+      let newDetails = [];
+      // console.log('data.details', data.details);
+      data.details.forEach(detail => {
+        newDetails.push(detail)
+      })
+      setVenetianDetails(newDetails);
+      setLoading(false);
+    });
+  }, [loading]);
+
+  console.log(venetianDetails);
+
+  // TODO: figure out why i cant grab the values I want from obj
   return (
     <div className="details-container">
-      <p>Details Placeholder text</p>
+      <div className='number-of-rooms-container'>
+        <h4>Number of rooms:</h4>
+      </div>
+      <div className='casino-container'>
+        <h4>Casino:</h4>
+      </div>
     </div>
   );
 }
