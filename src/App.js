6e6a2956135c7db62e3d13899f6cc3883eadcac1
William Y Wong <wwong1987@gmail.com>
2019-11-22 18:31:05 -0800

    created starting point for navbar component and displaying it on main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <main className="App">
       <body>
-        <h4>Welcome!</h4>
-        If you are here, then you are ready to start coding the example!
+        <Navbar/>
       </body>
     </main>
   );
