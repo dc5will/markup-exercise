7e03b9bddc0eacd57db51bd9ee38f91c0dafa444
William Y Wong <wwong1987@gmail.com>
2019-11-22 19:01:12 -0800

    relocated assets folder inside the scope of the project and rendering image in header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <main className="App">
       <body>
         <Navbar/>
+        <Header/>
       </body>
     </main>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+import Venetian from '../../assets/images/venetian.jpg'
+
+export default function Header(props) {
+  return (
+    <img
+      src={Venetian}
+      alt="venetian_image"
+    />
+  );
+}
