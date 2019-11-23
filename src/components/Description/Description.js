606c29d0f0d9732d5a697eae0a9a1256a008c27d
William Y Wong <wwong1987@gmail.com>
2019-11-23 14:27:20 -0800

    description component successfully fetching and rendering description from API

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,13 +1,24 @@
-import React from "react";
-import './Description.css'
+import React, { useState, useEffect } from "react";
+import HotelApiService from "../../services/hotel-api-service";
+import "./Description.css";
 
 export default function Description() {
+  const [venetianDescription, setVenetianDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    HotelApiService.getVenetianInfo().then(data => {
+      setVenetianDescription(data.description);
+      setLoading(false);
+    });
+  }, [loading]);
+
+  // console.log(venetianDescription)
+  // TODO: fix formatting for returned string
   
-
-
   return (
     <div className="description-container">
-      <p>Description Placeholder text</p>
+      <p>{venetianDescription}</p>
     </div>
   );
 }
