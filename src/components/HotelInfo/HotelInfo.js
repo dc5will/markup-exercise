6e867c0d6b9c018b667c643eebe247cd53d37e7a
William Y Wong <wwong1987@gmail.com>
2019-11-23 21:58:03 -0800

    fixed css styling for header. hotel info and price separated.

--- src/components/HotelInfo/HotelInfo.js
+++ src/components/HotelInfo/HotelInfo.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import HotelApiService from "../../services/hotel-api-service";
-import "./HotelInfo";
+import "./HotelInfo.css";
 
 export default function HotelInfo() {
   const [venetian, setVenetian] = useState([]);
   const [loading, setLoading] = useState(false);
+  // const [hotelName, setHotelName] = useState('');
 
   useEffect(() => {
     HotelApiService.getVenetianInfo().then(venetian => {
@@ -12,19 +13,20 @@ export default function HotelInfo() {
       setLoading(false);
     });
   }, [loading]);
-
-  console.log("venetian data", venetian);
-  console.log("venetian location", venetian.location);
-  console.log("venetian img", venetian.details);
+  
+  console.log("venetian data", typeof(venetian.name));
+  // console.log("venetian location", venetian.location);
+  // console.log("venetian img", venetian.details);
 
   return (
     <div className="header-info-container">
-      <div className="hotel-name">
-        <h4>{venetian.name} *****</h4>
-      </div>
+      <div className="hotel-name-container left-side">
+        <h4 className='hotel-name'>{venetian.name}</h4>
       <div>{venetian.phoneNumber}</div>
-      <div className='hotel-price'>
-        <h3>${venetian.price}</h3>
+      </div>
+
+      <div className='hotel-price right-side'>
+        <h4>${venetian.price}</h4>
       </div>
     </div>
   );
