5b62f27eb13968d7b88e8f8f1d8bd88506f2752b
William Y Wong <wwong1987@gmail.com>
2019-11-23 17:48:52 -0800

    completed requirements to remove duplicates and sort hotels in alphabetical order

--- src/components/HotelList/HotelList.js
+++ src/components/HotelList/HotelList.js
@@ -12,16 +12,30 @@ export default function HotelList() {
       hotels.list.forEach(hotel => {
         newHotels.push(hotel);
       });
+      
       // console.log(newHotels);
       setHotels(newHotels);
       setLoading(false);
     });
   }, [loading]);
 
-  console.log("testing", hotels);
+  // remove duplicates from hotel list
+  const uniqueHotelList = Array.from(new Set(hotels.map(a => a.name))).map(name => {
+    return hotels.find(a => a.name === name);
+  })
+  // console.log('uniqueHotelList', uniqueHotelList)
+  
+  // sort uniqueHotelList
+  const sortedHotelList = uniqueHotelList.sort((a, b) => (a.name > b.name) ? 1 : -1)
+  console.log('sortedHotelList', sortedHotelList)
+
+  // console.log('hotel list', hotels)
+  // const removeDuplicates = Array.from(new Set(hotels));
+  // console.log('remove duplicates', removeDuplicates)
+  
   return (
     <div className="hotel-list_container">
-      {hotels.map((hotel, index) => (
+      {sortedHotelList.map((hotel, index) => (
         <div key={index}>
           <div>
             <p>{hotel.name} - ${hotel.price}</p>
