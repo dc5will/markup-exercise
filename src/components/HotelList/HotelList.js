f84019b6d289c569a00f3092c2cfed006ddaf02b
William Y Wong <wwong1987@gmail.com>
2019-11-23 23:18:06 -0800

    hotel list column implemented, but still needs to be adjusted to proper ratio

--- src/components/HotelList/HotelList.js
+++ src/components/HotelList/HotelList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import HotelApiService from '../../services/hotel-api-service';
+import HotelApiService from "../../services/hotel-api-service";
 import "./HotelList.css";
 
 export default function HotelList() {
@@ -12,7 +12,7 @@ export default function HotelList() {
       hotels.list.forEach(hotel => {
         newHotels.push(hotel);
       });
-      
+
       // console.log(newHotels);
       setHotels(newHotels);
       setLoading(false);
@@ -20,25 +20,34 @@ export default function HotelList() {
   }, [loading]);
 
   // remove duplicates from hotel list
-  const uniqueHotelList = Array.from(new Set(hotels.map(a => a.name))).map(name => {
-    return hotels.find(a => a.name === name);
-  })
+  const uniqueHotelList = Array.from(new Set(hotels.map(a => a.name))).map(
+    name => {
+      return hotels.find(a => a.name === name);
+    }
+  );
   // console.log('uniqueHotelList', uniqueHotelList)
-  
+
   // sort uniqueHotelList
-  const sortedHotelList = uniqueHotelList.sort((a, b) => (a.name > b.name) ? 1 : -1)
-  console.log('sortedHotelList', sortedHotelList)
+  const sortedHotelList = uniqueHotelList.sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
+  console.log("sortedHotelList", sortedHotelList);
 
   // console.log('hotel list', hotels)
   // const removeDuplicates = Array.from(new Set(hotels));
   // console.log('remove duplicates', removeDuplicates)
-  
+
   return (
     <div className="hotel-list_container">
       {sortedHotelList.map((hotel, index) => (
         <div key={index}>
-          <div>
-            <p>{hotel.name} - ${hotel.price}</p>
+          <div className="row">
+            <div className="column">
+              <div className="left-column">{hotel.name}</div>
+            </div>
+            <div className="column">
+              <div className="right-column">${hotel.price}</div>
+            </div>
           </div>
         </div>
       ))}
