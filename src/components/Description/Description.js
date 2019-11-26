871081a10be295abc0f4bc25d5b2f214585d7188
William Wong <williamwdev@gmail.com>
2019-11-25 23:13:17 -0800

    experimenting with react-collapse on description component

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import HotelApiService from "../../services/hotel-api-service";
+import useCollapse from "react-collapsed";
 import "./Description.css";
 
 export default function Description() {
   const [venetianDescription, setVenetianDescription] = useState("");
-  
+
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -14,11 +15,48 @@ export default function Description() {
     });
   }, [loading]);
 
+  const {
+    getCollapseProps: outerCollapseProps,
+    getToggleProps: outerToggleProps,
+    isOpen: outerOpen
+  } = useCollapse({
+    defaultOpen: true
+  });
+  const {
+    getCollapseProps: innerCollapseProps,
+    getToggleProps: innerToggleProps,
+    isOpen: innerOpen
+  } = useCollapse();
+
+  // console.log('venetianDescription', venetianDescription)
+
   return (
     <div className="description-container">
-      <div className="hotel-description-collapsed">
+      <React.Fragment>
+        {/* <button {...outerToggleProps({ style: { marginBottom: "1em" } })}>
+          {outerOpen ? "Close" : "Expand"}
+        </button> */}
+        <section
+          className="hotel-description-collapsed"
+          {...outerCollapseProps()}
+        >
+          <p className="hotel-description">{venetianDescription}</p>
+
+          {!innerOpen && (
+            <button {...innerToggleProps()}>VIEW MORE DETAILS</button>
+          )}
+          <p {...innerCollapseProps()}>{venetianDescription}</p>
+          {innerOpen && (
+            <button {...innerToggleProps({ style: { display: "block" } })}>
+              Click to collapse
+            </button>
+          )}
+        </section>
+      </React.Fragment>
+
+      {/* <div className="hotel-description-collapsed">
         <p className="hotel-description">{venetianDescription}</p>
-      </div>
+      </div> */}
       {/* <div className="hotel-description-full">
         <p className="hotel-description">{venetianDescription}</p>
       </div> */}
