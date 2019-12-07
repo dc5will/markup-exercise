2685fe8c617e473baa0f733940cbaa84c521694c
William Wong <wwong1987@gmail.com>
2019-12-06 23:27:19 -0800

    refactoring state to hotelPage.js

--- src/components/HotelPage/HotelPage.js
+++ src/components/HotelPage/HotelPage.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import HotelList from "../HotelList/HotelList";
+import HotelApiService from '../../services/hotel-api-service';
 
 import "./HotelPage.css";
 
 export default function HotelPage() {
+  const [venetian, setVenetian] = useState([]);
+  const [hotelRating, setHotelRating] = useState("");
+  const [venetianLocation, setVenetianLocation] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    HotelApiService.getVenetianInfo().then(venetian => {
+      setVenetian(venetian);
+      setVenetianLocation(venetian.location);
+      setHotelRating(venetian.starRating);
+      setLoading(false);
+    });
+  }, [loading]);
 
   return (
     <>
@@ -22,7 +36,7 @@ export default function HotelPage() {
         {/* main content right side */}
         <main className="hotel-page-content">
           <header className="hotel-page-header-container">
-            <Header />
+            <Header venetian={venetian} hotelRating={hotelRating} venetianLocation={venetianLocation}/>
           </header>
         </main>
       </div>
