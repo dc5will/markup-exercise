d716ca72f8ee78a29b8b1127538af82e242c5f60
William Wong <wwong1987@gmail.com>
2019-12-08 23:13:53 -0800

    created context and set up basic boilerplate

---
+++ src/contexts/hotelContext.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+const HotelContext = React.createContext({
+  error: null,
+  setError: () => {},
+  clearError: () => {}
+});
+
+export default HotelContext;
+
+export class HotelProvider extends Component {
+  constructor(props) {
+    super(props);
+    const state = { user: {}, error: null };
+    this.state = state;
+  }
+
+  setError = error => {
+    console.error(error);
+    this.setState({ error });
+  };
+
+  clearError = () => {
+    this.setState({ error: null });
+  };
+
+  render() {
+    const value = {
+      error: this.state.error,
+      setError: this.setError,
+      clearError: this.clearError
+    };
+    return (
+      <HotelContext.Provider value={value}>
+        {this.props.children}
+      </HotelContext.Provider>
+    );
+  }
+}
